2e91c23edc019cbd7f1a76d10105c2d15e96eead
Aizzan96 <aizzan77@gmail.com>
2023-05-26 14:57:44 +0800

    layouts.dashboard template done

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -52,158 +52,21 @@
 
 <body>
     {{-- SIDEBAR --}}
-    <div class="sidebar sidebar-dark sidebar-fixed" id="sidebar">
-        <div class="sidebar-brand d-none d-md-flex">
-            <svg class="sidebar-brand-full" width="118" height="46" alt="CoreUI Logo">
-                <use xlink:href="assets/brand/coreui.svg#full"></use>
-            </svg>
-            <svg class="sidebar-brand-narrow" width="46" height="46" alt="CoreUI Logo">
-                <use xlink:href="assets/brand/coreui.svg#signet"></use>
-            </svg>
-        </div>
-        <ul class="sidebar-nav" data-coreui="navigation" data-simplebar="">
-            <li class="nav-item"><a class="nav-link" href="index.html">
-                    <svg class="nav-icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
-                    </svg> Dashboard<span class="badge badge-sm bg-info ms-auto">NEW</span></a></li>
-            <li class="nav-title">Theme</li>
-            <li class="nav-item"><a class="nav-link" href="colors.html">
-                    <svg class="nav-icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-drop"></use>
-                    </svg> Colors</a></li>
-            <li class="nav-title">Components</li>
-            <li class="nav-group"><a class="nav-link nav-group-toggle" href="">
-                    <svg class="nav-icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-puzzle"></use>
-                    </svg> Base</a>
-                <ul class="nav-group-items">
-                    <li class="nav-item"><a class="nav-link" href="base/accordion.html"><span class="nav-icon"></span>
-                            Accordion</a></li>
-                    <li class="nav-item"><a class="nav-link" href="base/breadcrumb.html"><span class="nav-icon"></span>
-                            Breadcrumb</a></li>
-                    <li class="nav-item"><a class="nav-link" href="base/cards.html"><span class="nav-icon"></span>
-                            Cards</a>
-                    </li>
-                </ul>
-            </li>
-            <li class="nav-group"><a class="nav-link nav-group-toggle" href="">
-                    <svg class="nav-icon">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-cursor"></use>
-                    </svg> Buttons</a>
-                <ul class="nav-group-items">
-                    <li class="nav-item"><a class="nav-link" href="buttons/buttons.html"><span class="nav-icon"></span>
-                            Buttons</a></li>
-                    <li class="nav-item"><a class="nav-link" href="buttons/button-group.html"><span
-                                class="nav-icon"></span>
-                            Buttons Group</a></li>
-
-                </ul>
-            </li>
-        </ul>
-        <button class="sidebar-toggler" type="button" data-coreui-toggle="unfoldable"></button>
-    </div>
+    @include('layouts.sidebar')
     {{-- SIDEBAR END --}}
 
-    {{-- Top part start--}}
+
     <div class="wrapper d-flex flex-column min-vh-100 bg-light">
+        {{-- Top part start--}}
+        {{-- Header --}}
         <header class="header header-sticky mb-4">
             {{-- navbar start --}}
-            <div class="container-fluid">
-                {{-- TOGGLE BUTTON FOR SIDEBAR --}}
-                <button class="header-toggler px-md-0 me-md-3" type="button"
-                    onclick="coreui.Sidebar.getInstance(document.querySelector('#sidebar')).toggle()">
-                    <svg class="icon icon-lg">
-                        <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-menu"></use>
-                    </svg>
-                </button><a class="header-brand d-md-none" href="#">
-                    {{-- TOGGLE BUTTON FOR SIDEBAR END --}}
-                    <svg width="118" height="46" alt="CoreUI Logo">
-                        <use xlink:href="assets/brand/coreui.svg#full"></use>
-                    </svg></a>
-                {{-- Left navbar --}}
-                <ul class="header-nav d-none d-md-flex">
-                    <li class="nav-item"><a class="nav-link" href="#">Dashboard</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Users</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">Settings</a></li>
-                </ul>
-                {{-- left navbar end --}}
-                {{-- right nav bar --}}
-                <ul class="header-nav ms-auto">
-                    <li class="nav-item"><a class="nav-link" href="#">
-                            <svg class="icon icon-lg">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-                            </svg></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">
-                            <svg class="icon icon-lg">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-list-rich"></use>
-                            </svg></a></li>
-                    <li class="nav-item"><a class="nav-link" href="#">
-                            <svg class="icon icon-lg">
-                                <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-                            </svg></a></li>
-                </ul>
-                {{-- right navbar ends --}}
-                {{-- profile navbar --}}
-                <ul class="header-nav ms-3">
-                    <li class="nav-item dropdown"><a class="nav-link py-0" data-coreui-toggle="dropdown" href="#"
-                            role="button" aria-haspopup="true" aria-expanded="false">
-                            <div class="avatar avatar-md"><img class="avatar-img" src="assets/img/avatars/8.jpg"
-                                    alt="user@email.com"></div>
-                        </a>
-                        {{-- choices underneath profile --}}
-                        <div class="dropdown-menu dropdown-menu-end pt-0">
-                            <div class="dropdown-header bg-light py-2">
-                                <div class="fw-semibold">Account</div>
-                            </div><a class="dropdown-item" href="#">
-                                <svg class="icon me-2">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-bell"></use>
-                                </svg> Updates<span class="badge badge-sm bg-info ms-2">42</span></a><a
-                                class="dropdown-item" href="#">
-                                <svg class="icon me-2">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-envelope-open"></use>
-                                </svg> Messages<span class="badge badge-sm bg-success ms-2">42</span></a><a
-                                class="dropdown-item" href="#">
-                                <svg class="icon me-2">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-task"></use>
-                                </svg> Tasks<span class="badge badge-sm bg-danger ms-2">42</span></a>
-                            {{-- next section --}}
-                            <div class="dropdown-header bg-light py-2">
-                                <div class="fw-semibold">Settings</div>
-                            </div>
-                            <a class="dropdown-item" href="#">
-                                <svg class="icon me-2">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-credit-card"></use>
-                                </svg> Payments<span class="badge badge-sm bg-secondary ms-2">42</span></a>
-                            <a class="dropdown-item" href="#">
-                                <svg class="icon me-2">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-file"></use>
-                                </svg> Projects<span class="badge badge-sm bg-primary ms-2">42</span></a>
-                            <div class="dropdown-divider"></div><a class="dropdown-item" href="#">
-                                <svg class="icon me-2">
-                                    <use xlink:href="vendors/@coreui/icons/svg/free.svg#cil-lock-locked"></use>
-                                </svg> Lock Account</a>
-
-                        </div>
-                        {{-- choices underneath navbar done --}}
-                    </li>
-                </ul>
-                {{-- profile navbar ends --}}
-            </div>
+            @include('layouts.navbar')
             {{-- navbar ends --}}
             {{-- Header divider --}}
-            <div class="header-divider"></div>
-            <div class="container-fluid">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb my-0 ms-2">
-                        <li class="breadcrumb-item">
-                            <!-- if breadcrumb is single--><span>Home</span>
-                        </li>
-                        <li class="breadcrumb-item active"><span>Dashboard</span></li>
-                    </ol>
-                </nav>
-            </div>
             {{-- header divider ends --}}
         </header>
+        {{-- Header --}}
         {{-- top part ends --}}
 
         <div class="body flex-grow-1 px-3">
@@ -217,26 +80,16 @@
             </div>
 
         </div>
-        <footer class="footer">
-            <div><a href="https://coreui.io">CoreUI </a><a href="https://coreui.io">Bootstrap Admin Template</a>
-                ©
-                2022
-                creativeLabs.</div>
-            <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a>
-            </div>
-        </footer>
+        {{-- Footer start --}}
+        @include('layouts.footer')
+        {{-- Footer ends --}}
 
     </div>
-    <script src="{{asset('js/coreuijs/main.js')}}"></script>
+
     <!-- CoreUI and necessary plugins-->
+    <script src="{{asset('js/coreuijs/main.js')}}"></script>
     <script src="{{asset('js/coreuijs/coreui.bundle.min.js')}}"></script>
     <script src="{{asset('js/coreuijs/simplebar.min.js')}}"></script>
-    <!-- Plugins and scripts required by this view-->
-    <script src="{{asset('js/coreuijs/chart.min.js')}}"></script>
-    <script src="{{asset('js/coreuijs/coreui-chartjs.js')}}"></script>
-    <script src="{{asset('js/coreuijs/coreui-utils.js')}}"></script>
-
-
 </body>
 
 </html>
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div><a href="*">Aizzan</a>
+        ©
+        2023
+        InspiringInc.</div>
+    <div class="ms-auto">Powered by&nbsp;<a href="https://coreui.io/docs/">CoreUI UI Components</a>
+    </div>
+</footer>
